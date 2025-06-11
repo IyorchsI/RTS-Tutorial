05ba231e947ef2e20de603e5c95a3884c2a49593
JRosero <joanrosero@unicauca.edu.co>
2025-06-11 18:05:41 -0500

    Optimal Script

--- Assets/Scripts/Puzzle/OnPlayerEnters.cs
+++ Assets/Scripts/Puzzle/OnPlayerEnters.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34cdce80784d68b4087e7d3d55d6ca1e215901bf98730ff5606642c86c82ede1
-size 1378
+oid sha256:29895c92ee30e5ec8fdd559804b28ffc16618355bdc039d444c1a2f158c8420d
+size 1377
