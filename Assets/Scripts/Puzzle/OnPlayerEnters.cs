3d052ea9c993e94d92d7f5c1b7a7fd2bb9abba3a
JRosero <joanrosero@unicauca.edu.co>
2025-06-11 18:23:49 -0500

    ReducedCollider

--- Assets/Scripts/Puzzle/OnPlayerEnters.cs
+++ Assets/Scripts/Puzzle/OnPlayerEnters.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:29895c92ee30e5ec8fdd559804b28ffc16618355bdc039d444c1a2f158c8420d
-size 1377
+oid sha256:b136f58ab0369462d77a057da99f0e25feeec4b19f6ee742c40e1adbd34ccb11
+size 1353
