95f364b78a3da3a6cc31ffae6e9f478199f90333
JRosero <joanrosero@unicauca.edu.co>
2025-06-11 18:42:14 -0500

    No se

--- Assets/Scripts/Puzzle/OnPlayerEnters.cs
+++ Assets/Scripts/Puzzle/OnPlayerEnters.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b136f58ab0369462d77a057da99f0e25feeec4b19f6ee742c40e1adbd34ccb11
+oid sha256:bb4743667023c7ec66d6c4a5a01e0087620c66866591efc8bc8bbad1d17cd825
 size 1353
