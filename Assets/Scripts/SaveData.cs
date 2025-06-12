dde60c4e00cfbabb46838ad7b3311d6fff32577d
Guillermo1298 <79525539+Guillermo1298@users.noreply.github.com>
2025-06-12 13:57:02 -0500

    Save system

---
+++ Assets/Scripts/SaveData.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:57ac553c65e7cce20e3a4e0baa0906ba951c5b6dd10db588ba5ccf2db6d74bcb
+size 314
---
+++ Assets/Scripts/SaveGameManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f6830188d75a8ca769a13869148fb1be10d02589b9ead3dc1e25d07d46d55556
+size 1446
