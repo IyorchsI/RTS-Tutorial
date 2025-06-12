1bd0316ca4ed90633deb00c4b5c95894dde3ed72
JRosero <joanrosero@unicauca.edu.co>
2025-06-12 16:31:44 -0500

    FixTrap

--- Assets/Scripts/Puzzle/MoveToPoint.cs
+++ Assets/Scripts/Puzzle/MoveToPoint.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe9ab6548f1e6245e22ceeca2900db4dd22315d0eaf1147005436c8fdaaa7a41
-size 2360
+oid sha256:ba39d53d1f8d2f6ebd2aa35ffaa80c901c5e18e3fefa232f85de77560d4dcb61
+size 2312
--- Assets/Scripts/Puzzle/ShowUpElevator.cs
+++ Assets/Scripts/Puzzle/ShowUpElevator.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16fbcfd8df59c6026b50b924f806cb037c9c700ddaa5ed97c1708c2848f363e1
-size 1970
+oid sha256:63b030ece93867ac33ba2047c6934f159b5b6c55191e3b186dc4abec2a0a6a13
+size 1903
