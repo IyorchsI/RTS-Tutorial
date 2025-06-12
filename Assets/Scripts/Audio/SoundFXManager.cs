8e28065e459aa8c0eaa06726b491bfbf2ccab7db
JRosero <joanrosero@unicauca.edu.co>
2025-06-12 11:10:10 -0500

    SoundFXDoor

---
+++ Assets/Scripts/Audio/SoundFXManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:08df34f4cc1c0df9bfa1c4521af82f7d1c469127688d243fddba7d56973d199a
+size 741
--- Assets/Scripts/Puzzle/OnPlayerEnters.cs
+++ Assets/Scripts/Puzzle/OnPlayerEnters.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1695d7b183bdcb90c477029cb07e2448fd5f573ff8235a2eed5414feaadcccca
-size 1356
+oid sha256:77734864c995e5d6a931137982c859f40029ef09bc3fa3cb736aa02dbf370d57
+size 1529
