834129dde283a677c57a876003a36d23636ed746
JRosero <joanrosero@unicauca.edu.co>
2025-06-13 13:13:07 -0500

    Now enemies attacks the player

--- Assets/_Scripts/Unit/AttackController.cs
+++ Assets/_Scripts/Unit/AttackController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bed0ea44b9f7909111a0105e3bba6481a3cbd71766b02d536f6a5232f791438c
-size 1902
+oid sha256:7b98832d4c8b3cca9dc169c29de03e5f248c7b4106eb0a6a7f12f0d7a003ab7c
+size 2348
