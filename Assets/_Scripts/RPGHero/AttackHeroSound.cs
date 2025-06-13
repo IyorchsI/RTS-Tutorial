b0f5ed1b70460663d04bd0302307d0c5e53f4132
JRosero <joanrosero@unicauca.edu.co>
2025-06-13 16:41:17 -0500

    Sound with animator

---
+++ Assets/_Scripts/RPGHero/AttackHeroSound.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1c5c1fd1921f50e5c313b986f605ab639f4cbc5f2fe4d6b7c88ed4cefa8f42cc
+size 1734
--- Assets/_Scripts/Unit/UnitAttackState.cs
+++ Assets/_Scripts/Unit/UnitAttackState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:426c85c72a83e7ad4f0a81c4b251f528e5e85cc5aae619649bcb243c0c5a6f37
-size 2592
+oid sha256:5e27da208b36843d0f658841e9e30cb5b96472d757c4f1c032e9631cf74b80ae
+size 2301
