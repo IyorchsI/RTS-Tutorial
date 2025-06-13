9649bc6b8aca3cfe25e1264ee244948b1fe36fbc
JRosero <joanrosero@unicauca.edu.co>
2025-06-13 18:19:51 -0500

    SimplePatrolFixFinal

--- Assets/_Scripts/Unit/SimplePatrol.cs
+++ Assets/_Scripts/Unit/SimplePatrol.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6df3728ab7d47c870529c82c4d757b8d6697929d13baa38895ded933d93ccb9
-size 1587
+oid sha256:982d9c2eafdd4ef96941470a841aa1a587e158ade1f251a7776973def7542f3d
+size 1627
