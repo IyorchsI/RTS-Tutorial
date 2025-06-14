e35973417fa9203628dfb5950c3b3914adcadf06
Dufray93 <DFRAY.93@GMAIL.COM>
2025-06-14 14:32:03 -0500

    feat: se añade inventario e items

---
+++ Assets/_Scripts/Unit/Item.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ea6f1200647b6b0ab25640635369396d993b25b630e453a11e2a9e5cbf2f4137
+size 1458
---
+++ Assets/_Scripts/Unit/Slot.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0225ceb757f9850b67a04c07649086c6c7dcdb850b9e3d635dce2b376715fb77
+size 735
