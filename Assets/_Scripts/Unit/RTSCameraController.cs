e5b914191f4aa701625b29e96cc7122eda5e177e
JRosero <joanrosero@unicauca.edu.co>
2025-06-14 15:51:13 -0500

    CameraFollowingAgain

--- Assets/_Scripts/Unit/RTSCameraController.cs
+++ Assets/_Scripts/Unit/RTSCameraController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe6a552559eaa0cc47652c418a4aacb104c0acc09ab27cb46a7518b0f7155dd4
-size 7445
+oid sha256:311c13251abd525fe2bde16cbd473357cc3c1a2f5f226659a4d9a20a7ddfb349
+size 7729
